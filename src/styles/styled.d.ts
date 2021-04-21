9120931fa51a3dd2400e0ca897612dc125997f94
Thales Macena <thalesmacena@gmail.com>
2021-04-20 22:41:34 -0300

    Define themes

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,20 @@
+import 'styled-components';
+
+interface Theme {
+  colors: {
+    background: string;
+    inputBackground: string;
+    primary: string;
+    text: string;
+    textInBackground: string;
+    switch: string;
+  };
+  background: string;
+  backgroundContainer: string;
+  boxShadow: string;
+  border: string;
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
---
+++ src/styles/themes.ts
@@ -0,0 +1,31 @@
+export const lightTheme = {
+  colors: {
+    background: '#fff',
+    inputBackground: '#FCFDFD',
+    primary: '#1f2687',
+    text: 'black',
+    textInBackground: '#1F2687',
+    switch: '#fff'
+  },
+  background:
+    'linear-gradient(90deg, rgba(224,228,231,1) 0%, rgba(45,48,92,1) 100%)',
+  backgroundContainer: 'rgba(93,36,175,0.25)',
+  boxShadow: '0 8px 32px 0 rgba(31,38,135,0.37)',
+  border: '1px solid rgba(255,255,255,0.18)'
+};
+
+export const darkTheme = {
+  colors: {
+    background: '#fff',
+    inputBackground: '#1E1E1E',
+    primary: '#1f1b24',
+    text: '#fff',
+    textInBackground: '#A56C47',
+    switch: '#057105'
+  },
+  background:
+    'linear-gradient(90deg, rgba(18,18,18,1) 0%, rgba(15,15,15,1) 100%);',
+  backgroundContainer: 'rgba(55,55,55,0.25)',
+  boxShadow: '0 8px 32px 0 rgba(31,38,135,0.37)',
+  border: '1px solid rgba( 255, 255, 255, 0.18 )'
+};
