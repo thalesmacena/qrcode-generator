696c97e9482a03c80129e44707ae7f810935eb68
Thales Macena <thalesmacena@gmail.com>
2021-04-20 22:42:12 -0300

    Create Theme Context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,48 @@
+import Cookies from 'js-cookie';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { ThemeProvider as StyledComponentThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/global';
+import { darkTheme, lightTheme } from '../styles/themes';
+
+interface ThemeContextData {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeContext = createContext({} as ThemeContextData);
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState(null);
+
+  const toggleTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
+  useEffect(() => {
+    const storedTheme = Cookies.get('fastfeet/theme');
+    setTheme(storedTheme || 'light');
+  }, []);
+
+  useEffect(() => {
+    Cookies.set('fastfeet/theme', theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <StyledComponentThemeProvider
+        theme={theme === 'dark' ? darkTheme : lightTheme}
+      >
+        <GlobalStyles />
+        {children}
+      </StyledComponentThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
