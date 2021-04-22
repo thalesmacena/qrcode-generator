3f6339ba68fbf511feb85e1e9810b3acbeaabccc
Thales Macena <thalesmacena@gmail.com>
2021-04-22 17:15:00 -0300

    add useCallback

--- src/components/InputText/InputText.tsx
+++ src/components/InputText/InputText.tsx
@@ -1,5 +1,13 @@
 import { ThemeContext } from '@/contexts/ThemeContext';
-import { Dispatch, SetStateAction, useContext, useMemo } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react';
 import JSONInput from 'react-json-editor-ajrm';
 import locale from 'react-json-editor-ajrm/locale/pt';
 import { ContainerInputText } from './styles';
@@ -12,6 +20,15 @@ interface IInputText {
 
 const InputText = ({ option, content, setContent }: IInputText) => {
   const { theme } = useContext(ThemeContext);
+  const [inputTheme, setInputTheme] = useState('dark_vscode_tribute');
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      setInputTheme('dark_vscode_tribute');
+    } else {
+      setInputTheme('light_mitsuketa_tribute');
+    }
+  }, [theme]);
 
   const JsonSample = useMemo(() => {
     return {
@@ -20,24 +37,28 @@ const InputText = ({ option, content, setContent }: IInputText) => {
     };
   }, []);
 
-  const handleJson = (value) => {
-    if (value.jsObject) {
-      setContent(JSON.stringify(value.jsObject));
-    }
-  };
+  const handleJson = useCallback(
+    (value) => {
+      if (value.jsObject) {
+        setContent(JSON.stringify(value.jsObject));
+      }
+    },
+    [setContent]
+  );
 
-  const handlePlainText = (e) => {
-    setContent(e.target.value);
-  };
+  const handlePlainText = useCallback(
+    (e) => {
+      setContent(e.target.value);
+    },
+    [setContent]
+  );
 
   return (
     <ContainerInputText>
       {option === 'JSON' ? (
         <JSONInput
           placeholder={JsonSample}
-          theme={
-            theme === 'dark' ? 'dark_vscode_tribute' : 'light_mitsuketa_tribute'
-          }
+          theme={inputTheme}
           locale={locale}
           onChange={handleJson}
           width={400}
